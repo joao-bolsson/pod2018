e45c184c4d6dea01bf281d081b8ca771fdd4272b
Joao Bolsson <joaovictorbolsson@gmail.com>
2018-05-15 14:42:53 -0300

    adiciona arquivos de entrada e saida e implementa funcao na main

--- t1/main.cpp
+++ t1/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Sorter.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+int main(int argc, char **argv) {
+    try {
+        if (argc == 5) {
+            Sorter sorter = Sorter(stoi(argv[1]), stoi(argv[2]));
+
+            sorter.classification(argv[3]);
+            sorter.intercalation(argv[4]);
+        }
+    } catch (exception& e) {
+        cout << "Erro: " << e.what() << endl;
+    }
     return 0;
 }
