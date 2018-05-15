d702fd14f9e77610dc73ef81a9d7ddcfd86cce69
Joao Bolsson <joaovictorbolsson@gmail.com>
2018-05-15 13:51:23 -0300

    define funcao para limpar arquivos (revisar)

--- t1/Sorter.cpp
+++ t1/Sorter.cpp
@@ -15,3 +15,16 @@ Sorter::Sorter(int memory, int numFiles) {
     }
     w = numFiles / 2;
 }
+
+void Sorter::clear(bool all) {
+    string out;
+    for (unsigned int i = 0; i < w; i++) {
+        out = (alternate ? "output" : "input") + to_string(i) + ".txt";
+        remove(out.c_str());
+
+        if (all) {
+            out = (alternate ? "input" : "output") + to_string(i) + ".txt";
+            remove(out.c_str());
+        }
+    }
+}
